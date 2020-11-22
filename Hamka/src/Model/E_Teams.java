606f81f2ff247eaec6b455346e408c41b5cee7d4
nagwan192 <69629529+nagwan192@users.noreply.github.com>
2020-11-22 19:57:41 +0200

    Sysdata//parser

---
+++ Hamka/src/Model/E_Teams.java
@@ -0,0 +1,7 @@
+package Model;
+
+public enum E_Teams {
+	Chimp,Crocodile,Scorpion,Giraffe,Spider,Viper,Panther,Zebra,Wolf,Sloth,Lion,Jellyfish,Panda,Piranha,Rabbit,
+	Hawk,Hedgehog,Husky,AQUA,Shark
+
+}
---
+++ Hamka/src/Model/E_UserType.java
@@ -0,0 +1,11 @@
+package Model;
+
+/**
+ * This enum represents the types of user that can use the game.
+ * helpful with user's permissions.
+ */
+public enum E_UserType {
+   
+	ADMIN,
+	PLAYER;
+}
