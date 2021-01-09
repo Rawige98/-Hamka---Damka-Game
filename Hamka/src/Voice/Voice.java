b9d17e6b228c927cf47646b0b8ef89780e337663
Amneh Hassan <amneh.hassan@gmail.com>
2021-01-09 20:07:39 +0200

    Voice Thread

--- Hamka/src/Voice/Voice.java
+++ Hamka/src/Voice/Voice.java
@@ -1,17 +1,34 @@
 package Voice;
 
-public class Voice {
-
-	public static void voicesExamples(String text) {
-		TextToSpeech tts = new TextToSpeech();
-
+public class Voice extends Thread{
 	
-
+	private String str;
+	
+	public Voice(String str) {
+		this.str = str;
+	}
+	
+	
+	
+	public static void doSpeak(String text) {
+		TextToSpeech tts = new TextToSpeech();
+	
 		// Setting the Voice
-		tts.setVoice("dfki-poppy-hsmm");
+		tts.setVoice("cmu-rms-hsmm");
 
 		// TTS say something that we actually are typing into the first variable
 		tts.speak(text, 2.0f, false, true);
 
 	}
+	
+	@Override
+	public void run() {
+		TextToSpeech tts = new TextToSpeech();
+		
+		// Setting the Voice
+		tts.setVoice("cmu-rms-hsmm");
+
+		// TTS say something that we actually are typing into the first variable
+		tts.speak(str, 2.0f, false, true);
+	}	
 }
